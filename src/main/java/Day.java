282f8de76dbbc4b80a79b6dbaaac801db3b8bb60
ethanGitStuff <gatescethan@gmail.com>
2026-03-17 15:32:59 -0500

    - Added Day Class

---
+++ src/main/java/Day.java
@@ -0,0 +1,9 @@
+public class Day {
+    public String name;
+    public int tempLow;
+    public int tempHigh;
+    public int precip;
+    public int windLow;
+    public int windHigh;
+    public String windDir;
+}
