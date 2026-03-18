f4e91a243f42a4ca33dd5c9c27994daa6e1ef911
ethanGitStuff <gatescethan@gmail.com>
2026-03-18 16:47:02 -0500

    - Added a way to get the WFO given lat and long from the cities_clean JSON file, using the MyWeatherApi class. Given two floats as input, it returns an ArrayList<String> of the WFO, gridX, gridY

--- src/main/java/MyWeatherAPI.java
+++ src/main/java/MyWeatherAPI.java
@@ -1,4 +1,37 @@
+import com.fasterxml.jackson.databind.JsonNode;
 import weather.WeatherAPI;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MyWeatherAPI extends WeatherAPI {
+
+    public ArrayList<String> getRegion(float lat, float lon) throws NullPointerException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.weather.gov/points/"+String.valueOf(lat)+","+String.valueOf(lon)))
+                .build();
+        HttpResponse<String> response = null;
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = null;
+
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            json = mapper.readTree(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> toRet = new ArrayList<>();
+        toRet.add(json.path("properties").path("gridId").asText());
+        toRet.add(String.valueOf(json.path("properties").path("gridX").asInt()));
+        toRet.add(String.valueOf(json.path("properties").path("gridY").asInt()));
+
+        return toRet;
+    }
+
 }
